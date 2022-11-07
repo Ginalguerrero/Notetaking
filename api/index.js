aa2f9b77a82abfe87e52503779aa475e8dafdaad
ginaguerrero25 <gina.l.guerrero@gmail.com>
2022-11-06 18:02:41 -0800

    notes saved

--- api/index.js
+++ api/index.js
@@ -1,21 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const { v4: uuid } = require('uuid');
-router.get("/notes", (req, res) => {
-    fs.readFile(`Develop/db/db.json`, (err, data) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        res.send(JSON.parse(data));
-    });
-})
+const fs = require("fs");
+const db = require('../Develop/db/db.json')
+
+router.get("/notes", (req, res) => res.json(db));
 router.post('/notes', (req, res) => {
-    const notes = req.body; 
-    console.log(notes)
-//    const data = JSON.parse(fs.readFileSync('Develop/db/db.json'));
-   // notes.id = uuid();
-  //  fs.writeFileSync('Develop/db/db.json', JSON.stringify(notes))
-  res.sendStatus(201)
+    const notes = req.body;
+    if (notes.text && notes.title) {
+        res.sendStatus(422);
+    }
+    notes.id = uuid();
+    db.push(notes);
+    fs.writeFileSync('../Develop/db/db.json', JSON.stringify(db, null, 2))
+    res.sendStatus(201)
 })
 module.exports = router
